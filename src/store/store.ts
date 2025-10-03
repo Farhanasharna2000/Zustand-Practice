d5b0fcb38f4b2405d8aabe8b960412e7e34a1431
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-10-03 10:44:53 +0600

    add logic in store  for addhabit

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,34 @@
 import { create } from "zustand";
 
-export interface Habit{
-    id:string;
-    name:string;
-    frequency:"daily"|"weekly";
-    completeDates:string[];
-    createdAt:string;
+export interface Habit {
+  id: string;
+  name: string;
+  frequency: "daily" | "weekly";
+  completeDates: string[];
+  createdAt: string;
 }
-interface HabitState{
-    habits:Habit[]
+interface HabitState {
+  habits: Habit[];
+  addHabit: (name: string, frequency: "daily" | "weekly") => void;
 }
-const useHabitStore=create<HabitState>()(()=>{
-    return{
-        habits:[],
-    }
-})
+const useHabitStore = create<HabitState>()((set) => {
+  return {
+    habits: [],
+    addHabit: (name, frequency) =>
+      set((state) => {
+        return {
+          habits: [...state.habits,
+            {
+              id: Date.now().toString(),
+              name,
+              frequency,
+              completeDates: [],
+              createdAt: new Date().toISOString(),
+            },
+          ],
+        };
+      }),
+  };
+});
 
 export default useHabitStore;
