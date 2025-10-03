fb47c37907457c66762ef42987052df513b5dd7b
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-10-03 13:15:21 +0600

    complete delete functionality

--- src/components/habit-list.tsx
+++ src/components/habit-list.tsx
@@ -4,20 +4,20 @@ import { Box, Button, Grid, Paper, Typography } from "@mui/material";
 import { CheckCircle, Delete } from "@mui/icons-material";
 
 const HabitList: React.FC = () => {
-  const { habits } = useHabitStore();
+  const { habits, removeHabit } = useHabitStore();
   const today = new Date().toISOString().split("T")[0];
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4 }}>
       {habits.map((habit) => (
         <Paper key={habit.id} elevation={2} sx={{ p: 2 }}>
-          <Grid container alignItems="center" >
-            <Grid size={{ xs: 12, sm:6}}>
+          <Grid container alignItems="center">
+            <Grid size={{ xs: 12, sm: 6 }}>
               <Typography variant="h6">{habit.name}</Typography>
               <Typography variant="body2" color="text.secondary">
                 {habit.frequency}
               </Typography>
             </Grid>
-            <Grid  size={{ xs: 12,sm:6 }}>
+            <Grid size={{ xs: 12, sm: 6 }}>
               <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}>
                 <Button
                   variant="outlined"
@@ -30,7 +30,12 @@ const HabitList: React.FC = () => {
                     ? "Completed"
                     : "Mark Complete"}
                 </Button>
-                <Button variant="outlined" color="error"    startIcon={<Delete />}>
+                <Button
+                  variant="outlined"
+                  color="error"
+                  startIcon={<Delete />}
+                  onClick={() => removeHabit(habit.id)}
+                >
                   Remove
                 </Button>
               </Box>
--- src/store/store.ts
+++ src/store/store.ts
@@ -11,25 +11,34 @@ export interface Habit {
 interface HabitState {
   habits: Habit[];
   addHabit: (name: string, frequency: "daily" | "weekly") => void;
+  removeHabit: (id: string) => void;
+  toggleHabit: (id: string, date: string) => void;
 }
-const useHabitStore = create<HabitState>()(devtools((set) => {
-  return {
-    habits: [],
-    addHabit: (name, frequency) =>
-      set((state) => {
-        return {
-          habits: [...state.habits,
-            {
-              id: Date.now().toString(),
-              name,
-              frequency,
-              completeDates: [],
-              createdAt: new Date().toISOString(),
-            },
-          ],
-        };
-      }),
-  };
-}));
+const useHabitStore = create<HabitState>()(
+  devtools((set) => {
+    return {
+      habits: [],
+      addHabit: (name, frequency) =>
+        set((state) => {
+          return {
+            habits: [
+              ...state.habits,
+              {
+                id: Date.now().toString(),
+                name,
+                frequency,
+                completeDates: [],
+                createdAt: new Date().toISOString(),
+              },
+            ],
+          };
+        }),
+      removeHabit: (id) =>
+        set((state) => ({
+          habits: state.habits.filter((habit) => habit.id !== id),
+        })),
+    };
+  })
+);
 
 export default useHabitStore;
